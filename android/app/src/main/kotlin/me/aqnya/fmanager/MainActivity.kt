7f8021d84aa7b1ee1c6a13400845cb828791fc4f
Nyawww <siyueba73@gmail.com>
2025-07-28 18:51:28 +0800

    update sdk.

--- android/app/src/main/kotlin/me/aqnya/fmanager/MainActivity.kt
+++ android/app/src/main/kotlin/me/aqnya/fmanager/MainActivity.kt
@@ -1,5 +1,5 @@
-package me.aqnya.fmanager
+package com.example.fmanager
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,18 +11,20 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:fmanager/main.dart';
 
 void main() {
-  testWidgets('Bottom navigation switches pages', (WidgetTester tester) async {
-    // 构建应用
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    expect(find.text('未安装'), findsOneWidget);
-    expect(find.text('设置页面'), findsNothing);
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
 
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
 
-    await tester.tap(find.text('设置'));
-    await tester.pumpAndSettle(); // 等待动画完成
-
-    expect(find.text('设置页面'), findsOneWidget);
-    expect(find.text('未安装'), findsNothing);
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
 }
