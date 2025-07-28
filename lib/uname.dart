8be8de65eac896f9f748e56e9e81dd81425c1f1c
Nyawww <siyueba73@gmail.com>
2025-07-28 16:49:20 +0800

    add uname

--- lib/uname.dart
+++ lib/uname.dart
@@ -2,14 +2,13 @@ import 'dart:io';
 
 Future<String> getKernelVersion() async {
   try {
-    final file = File('/proc/version');
-    if (await file.exists()) {
-      final content = await file.readAsString();
-      return content.trim();
+    final result = await Process.run('uname', ['-r']);
+    if (result.exitCode == 0) {
+      return result.stdout.toString().trim();
     } else {
-      return '无法访问 /proc/version';
+      return '获取失败: ${result.stderr}';
     }
   } catch (e) {
-    return '获取失败: $e';
+    return '异常: $e';
   }
 }
