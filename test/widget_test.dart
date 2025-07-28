d72d52eeb3e7dbe85e1e2f4d3fe8041296966d5a
Nyawww <siyueba73@gmail.com>
2025-07-28 18:57:59 +0800

    update dep

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,18 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:fmanager/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('Bottom navigation switches pages', (WidgetTester tester) async {
+    // 构建应用
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('未安装'), findsOneWidget);
+    expect(find.text('设置页面'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    await tester.tap(find.text('设置'));
+    await tester.pumpAndSettle(); // 等待动画完成
+
+    expect(find.text('设置页面'), findsOneWidget);
+    expect(find.text('未安装'), findsNothing);
   });
 }
