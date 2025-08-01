f777d4a22346bc2318968351c320767e73c444fa
Nyawww <siyueba73@gmail.com>
2025-08-02 02:42:12 +0800

    no warning.

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,21 +37,14 @@ val managerVersionCode by extra(getVersionCode())
 val managerVersionName by extra(getVersionName())
 
 fun getGitCommitCount(): Int {
-    val out = ByteArrayOutputStream()
-    project.exec {
-        commandLine("git", "rev-list", "--count", "HEAD")
-        standardOutput = out
-    }
-    return out.toString().trim().toInt()
+    val process = Runtime.getRuntime().exec("git rev-list --count HEAD")
+    val output = process.inputStream.bufferedReader().readText().trim()
+    return output.toInt()
 }
 
 fun getGitDescribe(): String {
-    val out = ByteArrayOutputStream()
-    project.exec {
-        commandLine("git", "describe", "--tags", "--always")
-        standardOutput = out
-    }
-    return out.toString().trim()
+    val process = Runtime.getRuntime().exec("git describe --tags --always")
+    return process.inputStream.bufferedReader().readText().trim()
 }
 
 fun getVersionCode(): Int {
