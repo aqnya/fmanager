b4e74e0836fb923b2e5713cca30836d5465f65da
Nyawww <siyueba73@gmail.com>
2025-08-02 00:26:14 +0800

    colorScheme.Variant.

--- lib/card.dart
+++ lib/card.dart
@@ -25,7 +25,7 @@ class InfoCard extends StatelessWidget {
               style: Theme.of(context)
                   .textTheme
                   .titleMedium
-                  ?.copyWith(color: colorScheme.onSurfaceVariant)),
+                  ?.copyWith(color: colorScheme.onSecondary)),
           const SizedBox(height: 8),
           ...children,
         ],
@@ -33,7 +33,7 @@ class InfoCard extends StatelessWidget {
     );
 
     return Card(
-      color: colorScheme.onSurface,
+      color: colorScheme.secondary,
       margin: const EdgeInsets.only(bottom: 16.0),
       child: onTap != null
           ? InkWell(
