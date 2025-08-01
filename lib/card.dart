95c9c0fda465ed921befed732574ba8e67253f11
Nyawww <siyueba73@gmail.com>
2025-08-01 16:42:46 +0800

    colorScheme.outlineVariant

--- lib/card.dart
+++ lib/card.dart
@@ -33,7 +33,7 @@ class InfoCard extends StatelessWidget {
     );
 
     return Card(
-      color: colorScheme.outlineVariant
+      color: colorScheme.outlineVariant,
       margin: const EdgeInsets.only(bottom: 16.0),
       child: onTap != null
           ? InkWell(
